cbbe104cd982480a8dc02efe748dd7720fb752d0
sjblake83 <sjblake83@gmail.com>
2025-09-30 23:17:15 -0400

    Added char count printout

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from stats import get_word_count
+from stats import get_word_count, get_character_counts
 
 def main():
     book_contents = get_book_text("books/frankenstein.txt")
     book_word_count = get_word_count(book_contents)
+    book_char_counts = get_character_counts(book_contents)
+    
     print(f"Found {book_word_count} total words")
+    print(book_char_counts)
 
 def get_book_text(book_path):
     with open(book_path) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def get_word_count(book_text):
     split_words = book_text.split()
     return len(split_words)
+
+def get_character_counts(book_text):
+    text_lower = book_text.lower()
+
+    char_dictionary = {}
+    
+    for char in text_lower:
+        if char not in char_dictionary:
+            char_dictionary.update({char : 1})
+        else:
+            char_dictionary[char] += 1
+    return char_dictionary
