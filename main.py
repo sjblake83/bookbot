3bf1ae3b73b8cd1199ed00f8897bf51943e385b4
sjblake83 <sjblake83@gmail.com>
2025-10-01 13:07:25 -0400

    Implement sorting, pretty print

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from stats import get_word_count, get_character_counts
 
 def main():
-    book_contents = get_book_text("books/frankenstein.txt")
-    book_word_count = get_word_count(book_contents)
-    book_char_counts = get_character_counts(book_contents)
+    book_path = "books/frankenstein.txt"
+    book_contents = get_book_text(book_path)
+ 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print(get_word_count(book_contents))
+    print(get_character_counts(book_contents))
+    print("============= END ===============")
     
-    print(f"Found {book_word_count} total words")
-    print(book_char_counts)
-
 def get_book_text(book_path):
     with open(book_path) as f:
         file_contents = f.read()
--- stats.py
+++ stats.py
@@ -1,15 +1,31 @@
 def get_word_count(book_text):
     split_words = book_text.split()
-    return len(split_words)
+    word_count_formatted = __format_word_count_for_printing(len(split_words))
+    return word_count_formatted
 
 def get_character_counts(book_text):
     text_lower = book_text.lower()
-
     char_dictionary = {}
-    
     for char in text_lower:
-        if char not in char_dictionary:
-            char_dictionary.update({char : 1})
-        else:
-            char_dictionary[char] += 1
-    return char_dictionary
+        char_dictionary[char] = char_dictionary.get(char, 0) + 1
+    char_count_sorted = __get_char_count_sorted_list(char_dictionary)
+    char_count_formatted = __format_char_count_for_printing(char_count_sorted)
+    return char_count_formatted
+
+def __get_char_count_sorted_list(char_dictionary):
+    chars_sorted = sorted(
+        list(char_dictionary.items()), 
+        key=lambda x: x[1], 
+        reverse=True)
+    return chars_sorted
+
+def __format_word_count_for_printing(word_count):
+    print_output = "----------- Word Count ----------\n"
+    print_output += f"Found {word_count} total words"
+    return print_output
+
+def __format_char_count_for_printing(char_counts):
+    print_output = ["--------- Character Count -------"]
+    for key, value in char_counts:
+        print_output.append(f"{key}: {value}")
+    return "\n".join(print_output)
